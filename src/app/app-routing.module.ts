cfd1efc8124ae8f6d3428734d27ee2ad3660d808
Vitor Vargem <vitorvargem27@gmail.com>
2023-07-24 17:03:48 -0300

    My first Angular Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { QuemSomosComponent } from './quem-somos/quem-somos.component';
+import { ContatoComponent } from './contato/contato.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  //Declarando a página home//
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+
+  {
+    path: 'quem-somos', component: QuemSomosComponent
+  },
+
+  {
+    path: 'contato', component: ContatoComponent
+  }
+
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
