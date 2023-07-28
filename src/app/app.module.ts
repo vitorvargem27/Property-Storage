a32664030488af02b300d2d1313b55f3bb75f0b2
Vitor Vargem <vitorvargem27@gmail.com>
2023-07-28 17:11:41 -0300

    Modifies 28/07

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { QuemSomosComponent } from './quem-somos/quem-somos.component';
 import { ContatoComponent } from './contato/contato.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './header/header.component';
 
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
@@ -15,9 +16,9 @@ import {MatToolbarModule} from '@angular/material/toolbar';
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
     QuemSomosComponent,
-    ContatoComponent
+    ContatoComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { NgFor } from '@angular/common';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatCardModule} from '@angular/material/card';
+import {MatIconModule} from '@angular/material/icon';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  standalone: true,
+  imports: [MatGridListModule, NgFor, MatCardModule,MatIconModule, MatDividerModule, MatButtonModule,],
 })
-export class HomeComponent {
 
+export class HomeComponent {
+  nome: string = 'Vitor';
+  imoveis: string[] = ['Casa Magnífica', 'Apartamento Padrão', 'Casa de Campo', 'Flat Minimalista', 'Sala Comercial', 'Cobertura Duplex'];
 }
